a845641a00a147ba4bb5e4306c3aed0b22fe76d2
TanZid-DarkLover <76641459+TanZid-DarkLover@users.noreply.github.com>
2021-01-09 23:46:44 +0600

    Update fbclone.py

--- fbclone.py
+++ fbclone.py
@@ -77,7 +77,7 @@ logo = """ ▄︻┻═┳一 W҈e҈l҈c҈o҈m҈e҈ T҈o҈ M҈y҈ T҈o҈o҈l҈
 \033[1;91m=======================================
 \033[1;96mAuthor  \033[1;93m: \033[1;92mTanzid Ahamed
 \033[1;96mTwitter \033[1;93m: \033[1: https://twitter.com/TanzidAhamed8/
-\033[1;96mFacebook  \033[1;93m:  \033[1:  https://facebook.com/Tanzid.Ahamed.707/
+\033[1;96mFacebook \033[1;93m: \033[1: https://facebook.com/Tanzid.Ahamed.707/
 \033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/TanZid-darkLover/
 \033[1;91m======================================="""
 
